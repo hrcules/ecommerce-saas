4b987a6fe551fb8728a457ac325f18ee4498f074
Hércules Costa <dev.hrcules@gmail.com>
2025-09-24 17:32:42 -0300

    feat: add category products page

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { categoryTable, productTable } from "@/db/schema";
+import { notFound } from "next/navigation";
+import Header from "@/components/common/header";
+import ProductItem from "@/components/common/product-item";
+
+interface CategoryPageProps {
+  params: { slug: string };
+}
+
+const CategoryPage = async ({ params }: CategoryPageProps) => {
+  const { slug } = await params;
+  const category = await db.query.categoryTable.findFirst({
+    where: eq(categoryTable.slug, slug),
+  });
+
+  if (!category) {
+    return notFound();
+  }
+
+  const products = await db.query.productTable.findMany({
+    where: eq(productTable.categoryId, category.id),
+    with: { variants: true },
+  });
+
+  return (
+    <>
+      <Header />
+
+      <div className="space-y-6 px-5">
+        <h2 className="text-xl font-semibold">{category.name}</h2>
+        <div className="grid grid-cols-2 gap-4">
+          {products.map((product, key) => (
+            <ProductItem product={product} key={key} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoryPage;
