12511641a8af992b9b6837b2a93f777067e090e1
Hércules Costa <dev.hrcules@gmail.com>
2026-03-09 14:48:49 -0300

    fix: migrating to the unified radix-ui package

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ChevronDownIcon } from "lucide-react";
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { Accordion as AccordionPrimitive } from "radix-ui";
 
 import { cn } from "@/lib/utils";
 
